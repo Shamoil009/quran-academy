f9ac1922c4bd6347ab2ad58d4e7ef074bdf0ee2a
shamoil009 <shamoilkhan888@gmail.com>
2023-09-23 22:43:51 +0500

    updated

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import Header from "../Header";
-
+import Image from "next/image";
+import PageTop from "@/public/images/ramadan.jpg";
 const Landing = () => {
   return (
     <div>
       <Header />
       <div>
-        <div>
-          <h1>Online Quran Learning Platform</h1>
-
-          <p>
-            An Online Quran Education platform, which provides the facility to
-            Learn Quran Online Islamic and Qur`anic teachings to Muslim students
-            across the world in four languages: English, Persian, Pashto, and
-            Urdu.
-          </p>
+        <div className="bg-tertiaryColor flex items-center justify-between px-10 py-10 text-textPrimaryColor">
+          <div className="w-[40%]">
+            <h1 className="text-5xl font-semibold">
+              Online Quran Learning Platform
+            </h1>
+            <p className="w-[90%] pt-5 text-3xl">
+              An Online Quran Education platform, which provides the facility to
+              Learn Quran Online Islamic and Quranic teachings to Muslim
+              students across the world in four languages: English, Persian,
+              Pashto, and Urdu.
+            </p>
+          </div>
+          <div className="w-[50%]">
+          <Image
+            src={PageTop}
+            height={2000}
+            width={2000}
+            className="w-full rounded-full"
+            alt=""
+          />
+          </div>
         </div>
       </div>
     </div>
