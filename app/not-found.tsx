15e026e6e89b039d4c53ecca69539867263cb2c6
shamoil009 <shamoilkhan888@gmail.com>
2023-10-25 11:25:05 +0500

    updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <div className="text-4xl font-bold">Page not found</div>
+      <div className="text-xl">
+        Looks like page you{`'`}re trying to access is not available
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
