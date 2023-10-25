f2e9a7c16eb9e89bec40b7f1b91e55e13b4c323f
shamoil009 <shamoilkhan888@gmail.com>
2023-10-25 11:25:40 +0500

    updated

--- utils/validation.ts
+++ utils/validation.ts
@@ -1,34 +1,20 @@
 import * as Yup from "yup";
 
 export const loginSchema = Yup.object({
-  //   name: Yup.string().min(2).max(25).required("Please enter your name"),
   email: Yup.string().email().required("Please enter your email"),
   password: Yup.string().min(4).required("Please enter your password"),
 });
 
 export const fillFormSchema = Yup.object({
-  fullName: Yup.string().min(1).required("name is required"),
+  fullName: Yup.string().min(2).required("name is required"),
   email: Yup.string().email().min(1).required("email is required"),
   gender: Yup.string().oneOf(["male", "female", "Rather not say"]),
   number: Yup.string()
-    .matches(/^\d{8,25}$/, "Number must be between 8 and 25 digits and no special characters like (+, -)")
+    .matches(
+      /^\d{8,25}$/,
+      "Number must be between 8 and 25 digits and no special characters like (+, -)",
+    )
     .required("whatsapp no is required"),
   country: Yup.string().min(1).required("country is required"),
   courses: Yup.string().min(1).required("select atleast one course"),
-});
-
-export const signupSchema = Yup.object({
-  //   name: Yup.string().min(2).max(25).required("Please enter your name"),
-  email: Yup.string().email().required("Please enter your email"),
-  password: Yup.string().min(8).required("Please enter your password"),
-  confirmPassword: Yup.string()
-    .required("Field is required")
-    .oneOf([Yup.ref("password")], "Password must match"),
-});
-
-export const otpSchema = Yup.object({
-  otp: Yup.string()
-    .min(6, "Enter 6 digits of otp")
-    .max(6, "Enter 6 digits of otp")
-    .required("Please enter otp"),
 });
