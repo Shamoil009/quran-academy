ff2e5119575915e31aa487b5aed43d5dd5c5ee05
shamoil009 <shamoilkhan888@gmail.com>
2023-10-28 14:51:33 +0500

    updated

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import About from "@/components/About";
+import Layout from "@/components/Layout";
+import React from "react";
+
+const page = () => {
+  return (
+    <Layout>
+      <About />
+    </Layout>
+  );
+};
+
+export default page;
