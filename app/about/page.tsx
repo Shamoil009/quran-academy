b0a74f0767ef95f792a49464fd1c90a92f30f190
shamoil009 <shamoilkhan888@gmail.com>
2023-10-30 10:20:49 +0500

    updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import Layout from "@/components/Layout";
+import Whatsapp from "@/components/Whatsapp";
 import React from "react";
 
 const page = () => {
   return (
     <Layout>
       <About />
+      <Whatsapp/>
     </Layout>
   );
 };
