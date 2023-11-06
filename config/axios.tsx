1876648e2f2719c4299d2ba07f4730be1cdd7ad0
shamoil009 <shamoilkhan888@gmail.com>
2023-11-06 10:46:31 +0500

    updated

--- config/axios.tsx
+++ config/axios.tsx
@@ -2,26 +2,17 @@ import axios from "axios";
 import { QA_TOKEN } from "@/utils/constants";
 
 const axiosInstance = () => {
-  
   const token = localStorage.getItem(QA_TOKEN);
 
   //setting the enviroment
   let url = "";
-  if (process.env.NEXT_PUBLIC_NODE_ENV === "local") {
+  if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
     url = process.env.NEXT_PUBLIC_LOCAL_BASE_URL || "";
-  } else if (process.env.NEXT_PUBLIC_NODE_ENV === "onmachine") {
-    url = process.env.NEXT_PUBLIC_DEVELOPMENT_BASE_URL || "";
-  } else if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
-    url = process.env.NEXT_PUBLIC_TEST_API_URL || "";
-  } else if (process.env.NEXT_PUBLIC_NODE_ENV === "staging") {
-    url = "http://143.244.191.170";
   } else if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
     // url = "http://backend.aqonlinequranacademy.com";
-    url = "https://backend.aqonlinequranacademy.com";
-
+    url = process.env.NEXT_PUBLIC_PRODUCTION_BASE_URL || "";
   }
 
-  // console.log(url);
   const enviroment = `${url}/api`;
   return axios.create({
     baseURL: enviroment,
