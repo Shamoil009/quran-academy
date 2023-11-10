38a995590e83651937441c755ba5e9c32be73094
shamoil009 <shamoilkhan888@gmail.com>
2023-11-10 17:38:01 +0500

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Providers } from "@/redux-store/provider";
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 
+
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
