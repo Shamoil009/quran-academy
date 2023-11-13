5df2854709bc624aaff5cdca55f76feacc67dccb
shamoil009 <shamoilkhan888@gmail.com>
2023-11-13 17:34:38 +0500

    updated

---
+++ app/pricing/page.tsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/Layout";
+import Pricing from "@/components/Pricing";
+import React from "react";
+
+const page = () => {
+  return (
+    <Layout>
+      <Pricing />
+    </Layout>
+  );
+};
+
+export default page;
